4664d6fceaef50a0ea4ac0a52245bbfbfb476f94
Samer Koda <SamerKoda@rocketcentral.com>
2023-06-28 10:51:58 -0400

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODM1OA/spectral.js?branch_slug=main&token=319d182f-5a3c-4e37-a52c-b57d49399db6";
+export default { extends: ruleset };
